b103ca6e9c573246a93e8359fc2ba1bc9e523b02
Berend <berend.vandenbussche@student.howest.be>
2022-05-26 19:27:00 +0200

    added weather station project with current temp

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const showWeatherData = (data) => {
+  document.querySelector('.js-temp').innerHTML = data + '°C';
+};
+
+const getData = (url, callback) => {
+  fetch(url)
+    .then(function (response) {
+      if (!response.ok) {
+        throw Error(`Problem with fetch(). Status Code: ${response.status}`);
+      } else {
+        console.info('Got response');
+        return response.json();
+      }
+    })
+    .then(function (jsonObject) {
+      console.info('Created json object');
+      console.info('Reading data');
+      callback(jsonObject);
+    })
+    .catch(function (error) {
+      console.error(`Error with json ${error}`);
+    });
+};
+
 const listenToMobileMenu = () => {
   const menu = document.querySelector('.js-mobile-menu');
   const container = document.getElementById('mobile-menu');
@@ -18,6 +42,7 @@ const listenToMobileMenu = () => {
 
 const init = () => {
   listenToMobileMenu();
+  getData('http://141.134.231.181:5500/api/v1/weather/temperature', showWeatherData);
 };
 
 document.addEventListener('DOMContentLoaded', function () {
